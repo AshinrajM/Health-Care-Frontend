8af656983b712266fa5406a01e3f95ee2afb11ac
AshinrajM <ashinrajeevank@outlook.com>
2024-01-31 10:04:07 +0530

    tailwind configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  // content: ["./src/*/.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#0067FF",
+        yellowColor: "#FEB60D",
+        purpleColor: "#9771FF",
+        irisBlueColor: "#01B5C5",
+        headingColor: "#181A1E",
+        textColor: "#4E545F",
+      },
+
+      boxShadow: {
+        panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
+      },
+    },
+  },
+  plugins: [],
+});
