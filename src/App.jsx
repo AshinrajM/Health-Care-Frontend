911f9752608ed1337173ccd2b38002e55c5790e0
AshinrajM <ashinrajeevank@outlook.com>
2024-02-02 11:57:36 +0530

    app wrapped with browser router in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/user/Home'
-import Signin from './pages/user/Signin'
-import Signup from './pages/user/Signup'
-import Header from './components/Header/Header'
-import AdminLogin from './pages/admin/AdminLogin'
+import { Route, Routes } from 'react-router-dom'
+import UserRoutes from './routes/UserRoutes'
+import AdminRoutes from './routes/AdminRoutes'
 
 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
+    <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<Signin />} />
-        <Route path='/sign-up' element={<Signup />} />
-        <Route path='/admin-log-in' element={<AdminLogin />} />
+        <Route path='/*' element={<UserRoutes />} />
+        <Route path='/admin/*' element={<AdminRoutes />} />
       </Routes>
-    </BrowserRouter>
+    </div >
   )
 }
