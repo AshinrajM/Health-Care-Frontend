1d0d674116adcb9f1b6b51ac8ba3daa1e4207cc7
AshinrajM <ashinrajeevank@outlook.com>
2024-03-13 21:27:20 +0530

    associates list displayed in admin side

--- src/pages/admin/AdminAssociates.jsx
+++ src/pages/admin/AdminAssociates.jsx
@@ -4,8 +4,26 @@ import { Button, Card, Typography } from '@material-tailwind/react'
 import { HiUserAdd } from "react-icons/hi";
 import SideBar from '../../components/Sidebar/SideBar'
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function AdminAssociates() {
+
+    const [associates, setAssociates] = useState([])
+
+    useEffect(() => {
+        async function getAssociates() {
+            try {
+                const associates = await axios.get('http://127.0.0.1:8000/users/associatelist')
+                console.log("associates", associates.data);
+                setAssociates(associates.data)
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getAssociates()
+    }, [])
     return (
         <div className='bg-blue-gray-500 flex flex-col lg:flex-row'>
             <div className='lg:w-64 flex-none'>
@@ -23,13 +41,26 @@ export default function AdminAssociates() {
                         <table className='text-black w-full'>
                             <thead className='text-black' style={{ borderBottom: '1px dotted' }}>
                                 <tr className='text-black'>
-                                    <th className='p-2'>#</th>
-                                    <th className='p-2'>Email</th>
-                                    <th className='p-2'>Status</th>
-                                    <th className='p-2'>Block/Unblock</th>
+                                    <th className='p-2'>sl.no:</th>
+                                    <th className='p-2'>Name</th>
+                                    <th className='p-2'>Certificate NO.</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                {
+                                    associates.map((associate, index) => (< tr key={associate.id || index} className='text-black' style={{ borderBottom: '1px dotted' }}>
+                                        <td className='p-2' style={{ textAlign: 'center' }}>{index + 1}</td>
+                                        <td className='p-2' style={{ textAlign: 'center' }}>{associate.name}</td>
+                                        <td className='p-2' style={{ textAlign: 'center' }}>{associate.certificate_no}</td>
+                                        {/* <td className='p-2' style={{ display: 'flex', justifyContent: 'center' }}>
+                                            {associate.is_active ? <FaLockOpen className="w-7 h-7" /> : <FaLock className="w-7 h-7" />}
+                                        </td> */}
+                                        {/* <td>{associate.is_active ? <Button color="red" size="sm">Block</Button> : <Button color="blue" size="sm">UnBlock</Button>}</td> */}
+                                    </tr>
+                                    ))
+                                }
+                            </tbody>
+                            {/* <tbody>
                                 <tr className='text-black' style={{ borderBottom: '1px dotted' }}>
                                     <td className='p-2' style={{ textAlign: 'center' }}>1</td>
                                     <td className='p-2' style={{ textAlign: 'center' }}>salman@example.com</td>
@@ -38,18 +69,17 @@ export default function AdminAssociates() {
                                         <FaLockOpen className="w-7 h-7" />
                                     </td>
                                 </tr>
-                            </tbody>
-                            <tbody>
-                                <tr className='text-black' >
-                                    <td className='p-2' style={{ textAlign: 'center' }}>2</td>
+                            </tbody> */}
+                            {/* <tbody>
+                                <tr className='text-black' style={{ borderBottom: '1px dotted' }}>
+                                    <td className='p-2' style={{ textAlign: 'center' }}>1</td>
                                     <td className='p-2' style={{ textAlign: 'center' }}>salman@example.com</td>
-                                    <td className='p-2' style={{ textAlign: 'center' }}>Blocked</td>
+                                    <td className='p-2' style={{ textAlign: 'center' }}>Active</td>
                                     <td className='p-2' style={{ display: 'flex', justifyContent: 'center' }}>
-                                        <FaLock className="w-7 h-7" />
+                                        <FaLockOpen className="w-7 h-7" />
                                     </td>
                                 </tr>
-                            </tbody>
-
+                            </tbody> */}
                         </table>
                     </div>
                 </Card>
--- src/pages/admin/AdminUsers.jsx
+++ src/pages/admin/AdminUsers.jsx
@@ -1,6 +1,6 @@
 import { FaLockOpen } from "react-icons/fa6";
 import { FaLock } from "react-icons/fa6";
-import { Card, Typography } from '@material-tailwind/react'
+import { Button, Card, Typography } from '@material-tailwind/react'
 import SideBar from '../../components/Sidebar/SideBar'
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -12,8 +12,9 @@ export default function AdminUsers() {
     useEffect(() => {
         async function getAllUser() {
             try {
-                const users = await axios.get('http://127.0.0.1:8000/users/register')
+                const users = await axios.get('http://127.0.0.1:8000/users/userslist')
                 console.log("sdsd", users.data);
+                // console.log("is", users.data.is_active);
                 setUsers(users.data)
 
             } catch (error) {
@@ -22,9 +23,6 @@ export default function AdminUsers() {
         }
         getAllUser()
     }, [])
-
-    let usersD = users.map((user) => user.email)
-    console.log('ddddd', usersD)
     return (
         <div className='bg-blue-gray-500 flex flex-col lg:flex-row'>
             <div className='lg:w-64 flex-none'>
@@ -40,40 +38,22 @@ export default function AdminUsers() {
                                     <th className='p-2'></th>
                                     <th className='p-2'>Email</th>
                                     <th className='p-2'>Status</th>
-                                    {/* <th className='p-2'>Block/Unblock</th> */}
                                 </tr>
                             </thead>
-                            {/* <tbody>
-                                {
-                                    users.map((user, index) => {
-                                        < tr key={user.id || index} className='text-black' style={{ borderBottom: '1px dotted' }}>
-                                            <td className='p-2' style={{ textAlign: 'center' }}>{index + 1}</td>
-                                            <td className='p-2' style={{ textAlign: 'center' }}>{user.email}</td>
-                                            <td className='p-2' style={{ display: 'flex', justifyContent: 'center' }}>
-                                                <FaLockOpen className="w-7 h-7" />
-                                            </td>
-                                        </tr>
-                                    })
-                                }
-                            </tbody> */}
                             <tbody>
-                                <tr className='text-black' >
-                                    <td className='p-2' style={{ textAlign: 'center' }}>1</td>
-                                    <td className='p-2' style={{ textAlign: 'center' }}>ajmal@gmail.com</td>
-                                    <td className='p-2' style={{ textAlign: 'center' }}>Block</td>
-                                    <td className='p-2' style={{ display: 'flex', justifyContent: 'center' }}>
-                                        <FaLockOpen className="w-7 h-7" />
-                                    </td>
-                                </tr>
-                                <tr className='text-black' >
-                                    <td className='p-2' style={{ textAlign: 'center' }}>2</td>
-                                    <td className='p-2' style={{ textAlign: 'center' }}>salman@example.com</td>
-                                    <td className='p-2' style={{ textAlign: 'center' }}>Block</td>
-                                    <td className='p-2' style={{ display: 'flex', justifyContent: 'center' }}>
-                                        <FaLockOpen className="w-7 h-7" />
-                                    </td>
-                                </tr>
+                                {
+                                    users.map((user, index) => (< tr key={user.id || index} className='text-black' style={{ borderBottom: '1px dotted' }}>
+                                        <td className='p-2' style={{ textAlign: 'center' }}>{index + 1}</td>
+                                        <td className='p-2' style={{ textAlign: 'center' }}>{user.email}</td>
+                                        <td className='p-2' style={{ display: 'flex', justifyContent: 'center' }}>
+                                            {user.is_active ? <FaLockOpen className="w-7 h-7" /> : <FaLock className="w-7 h-7" />}
+                                        </td>
+                                        <td>{user.is_active ? <Button color="red" size="sm">Block</Button> : <Button color="blue" size="sm">UnBlock</Button>}</td>
+                                    </tr>
+                                    ))
+                                }
                             </tbody>
+
 
                         </table>
 
@@ -86,4 +66,3 @@ export default function AdminUsers() {
         </div >
     )
 }
-
