d0db3dc780c207dfe739cdcd64f39655d25d7828
AshinrajM <ashinrajeevank@outlook.com>
2024-04-01 16:55:34 +0530

    associate page in user side

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -54,17 +54,19 @@ export default function Header() {
                 </div>
                 <div className='flex justify-center md:justify-end'>
                     <ul className='flex flex-col md:flex-row gap-4 md:gap-11 items-center justify-end font-serif text-xl'>
-                        {/* <Link to='/'>
-                            <li className={isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer>Home</li>
-                        </Link>
-                        <li className={isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer>Bookings</li>
-                        <li className={isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer>Associates</li>
-                        <li className={isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer>Contact Us</li> */}
                         <Link to='/'>
                             <li className={`${isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer`}>Home</li>
                         </Link>
                         <li className={`${isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer`}>Bookings</li>
-                        <li className={`${isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer`}>Associates</li>
+                        {isAuthenticated ? (
+                            <Link to='/secured/associate-list'>
+                                <li className={`text-blue-900 mb-2 md:mb-0 hover:cursor-pointer`}>Associates</li>
+                            </Link>
+                        ) : (
+                            <Link to='/signin'>
+                                <li className={`text-black mb-2 md:mb-0 hover:cursor-pointer`}>Associates</li>
+                            </Link>
+                        )}
                         <li className={`${isAuthenticated ? 'text-blue-900' : 'text-black'} mb-2 md:mb-0 hover:cursor-pointer`}>Contact Us</li>
 
 
--- src/pages/user/AssociateList.jsx
+++ src/pages/user/AssociateList.jsx
@@ -1,34 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import { Card, CardBody, CardFooter, Typography, Button, } from "@material-tailwind/react";
+import { Card, CardBody, CardFooter, Typography, Button, Input, Radio } from "@material-tailwind/react";
 
 
 const AssociateList = () => {
 
+    const [exp, setExp] = useState(0);
+    const [fee, setFee] = useState(0);
+    const[shift,setShift] = useState(null)
+
     const textStyle = {
         fontFamily: "Afacad",
-        fontSize: '20px',
+        fontSize: '14px',
         fontWeight: '550',
     }
+
     return (
         <>
             <div>
                 <Header />
             </div>
             <div>
-                <Card className="m-3 w-80 hover:shadow-xl bg-" >
+
+                <Card className="mt-6 bg-green-300  shadow-xl rounded-none">
+                    <CardBody className='flex space-x-5 items-center justify-center'>
+                        <div >
+                            <Input size="md" label="date" color='black' type='date' />
+                        </div>
+                        <div >
+                            <p className='text-black'> Fee/Hour: ₹ {fee}</p>
+                            <input type="range" min={0} max={1000} value={fee} step={100} className='range range-primary' onChange={(e) => setFee(parseInt(e.target.value))} />
+                        </div>
+                        <div >
+                            <p className='text-black'>Experience : {exp}yrs</p>
+                            <input type="range" min={0} max={5} value={exp} step={1} className='range range-primary' onChange={(e) => setExp(parseInt(e.target.value))} />
+                        </div>
+                        <div className='flex space-x-2'>
+                            <Radio color='green' name='color' label="Morning" />
+                            <Radio color='red' name='color' label="Noon" />
+                            <Radio color='indigo' name='color' label="Fullday" />
+                        </div>
+                        <div className=''>
+                            <Button variant='outlined' className='hover:bg-black hover:text-white rounded-md '>Search</Button>
+                        </div>
+                    </CardBody>
+                </Card>
+            </div>
+            <hr className='mt-5 mx-10' />
+            <div>
+                <Card className="m-5 w-72  hover:shadow-xl hover:scale-105 duration-1000" >
                     <CardBody>
-                        <Typography variant="h4" color="indigo" className="mb-2" style={{ fontFamily: 'Krona One' }}>
+                        <Typography variant="h5" color="indigo" className="mb-2" style={{ fontFamily: 'Krona One' }}>
                             Anwar Ali
                         </Typography>
-                        <div className='px-5 ' >
+                        <div className='px-3 ' >
                             <div className='flex justify-between text-black'>
                                 <Typography className='self-end ' style={textStyle}>
                                     Age
                                 </Typography>
                                 <Typography className='' style={textStyle}>
-                                    : &nbsp; 26
+                                    &nbsp; 26 Yrs
                                 </Typography>
                             </div>
                             <div className='flex justify-between text-black'>
@@ -36,7 +68,7 @@ const AssociateList = () => {
                                     Experience
                                 </Typography>
                                 <Typography style={textStyle}>
-                                    : &nbsp; 26
+                                    &nbsp; 2 Yrs
                                 </Typography>
                             </div>
                             <div className='flex justify-between text-black' >
@@ -44,7 +76,31 @@ const AssociateList = () => {
                                     Fee per hour
                                 </Typography>
                                 <Typography style={textStyle}>
-                                    : &nbsp; 26
+                                    &nbsp; $ 260
+                                </Typography>
+                            </div>
+                            <div className='flex justify-between text-black' >
+                                <Typography className='self-end' style={textStyle}>
+                                    Date
+                                </Typography>
+                                <Typography style={textStyle}>
+                                    &nbsp; 26-10-2022
+                                </Typography>
+                            </div>
+                            <div className='flex justify-between text-black' >
+                                <Typography className='self-end' style={textStyle}>
+                                    Morning(08:00-12:00)
+                                </Typography>
+                                <Typography style={textStyle}>
+                                    &nbsp; Available
+                                </Typography>
+                            </div>
+                            <div className='flex justify-between text-black' >
+                                <Typography className='self-end' style={textStyle}>
+                                    Noon(01:00-05:00)
+                                </Typography>
+                                <Typography style={textStyle}>
+                                    &nbsp; Not Available
                                 </Typography>
                             </div>
                         </div>
