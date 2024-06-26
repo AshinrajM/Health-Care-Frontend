b25aa789766bf9738d70650940900093434503a6
AshinrajM <131889952+AshinrajM@users.noreply.github.com>
2024-06-26 13:15:17 +0530

    Update api

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -18,7 +18,7 @@ if (userToken) {
 console.log("Token being sent:", token);
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://127.0.0.1:8000',
+    
     baseURL: 'http://3.82.108.203/',
     headers: {
         'Content-Type': 'application/json',
